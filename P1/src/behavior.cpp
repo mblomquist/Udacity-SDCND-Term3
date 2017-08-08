02b395b697629e69dcfb77a434e7bf6533fb60b8
Matt Blomquist <blomquist.matt@gmail.com>
2017-08-08 06:57:19 -0700

    Update behavior.cpp

--- P1/src/behavior.cpp
+++ P1/src/behavior.cpp
@@ -1,1 +1,6 @@
 // Behavior Planner
+// 
+// Steps:
+// 1. Find all the cars given from the sensor fusion vector.
+// 2. Estimate where the cars will be within the trajectory distance (10 time steps?).
+// 3. Return estimates of where the cars will be.
