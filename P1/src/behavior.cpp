404777cde152cc9489523e399b6e7022fb9a0263
Matt <blomquist.matt@gmail.com>
2017-08-26 19:08:42 -0700

    Update: Remove unused files.

--- P1/src/behavior.cpp
+++
@@ -1,6 +0,0 @@
-// Behavior Planner
-// 
-// Steps:
-// 1. Read in the prediction data (see prediction.cpp) and waypoint information.
-// 2. Determine the action to take based on the read information.
-// 3. Return location to go.
--- P1/src/prediction.cpp
+++
@@ -1,6 +0,0 @@
-// Prediction Step - Temp
-// 
-// Steps:
-// 1. Find all the cars given from the sensor fusion vector.
-// 2. Estimate where the cars will be within the trajectory distance (10 time steps?).
-// 3. Return estimates of where the cars will be.
